0e8e06e3c42d3ea24ff0fcba5812abe697084193
Malika Bukenova <mb.bukenova@gmail.com>
2024-09-01 03:01:57 +0500

    fix: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  function check(str, bracketsConfig) {
+    const stack = [];
+    const brMap = new Map(bracketsConfig);
+
+    for (let char of str) {
+        if (brMap.has(char)) {
+            if (stack.length > 0 && stack[stack.length - 1] === char) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+        } else {
+            const lastOpening = stack.pop();
+            if (brMap.get(lastOpening) !== char) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
 }
