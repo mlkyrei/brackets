ed4423b51cb4fba98d763274deb02b70d836a410
Malika Bukenova <mb.bukenova@gmail.com>
2024-09-01 03:08:47 +0500

    fix: change codes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   const stack = [];
-  const bracketMap = new Map(bracketsConfig.map(([open, close]) => [close, open]));
+  const bracketMap = new Map(bracketsConfig.map(([open, close]) => [open, close]));
 
   for (let char of str) {
       if (bracketMap.has(char)) {
-          const lastOpening = stack.pop();
-          if (bracketMap.get(char) !== lastOpening) {
-              return false;
-          }
-      } else {
-          if (stack.length > 0 && stack[stack.length - 1] === char) {
+          if (char === bracketMap.get(char) && stack.length > 0 && stack[stack.length - 1] === char) {
               stack.pop();
           } else {
               stack.push(char);
+          }
+      } else {
+          if (stack.length === 0 || bracketMap.get(stack.pop()) !== char) {
+              return false;
           }
       }
   }
